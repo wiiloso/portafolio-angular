916ce0ebedda2af08f4fcf3d334dff0abcc7e307
wiloso <wiilos@hotmail.com>
2019-02-07 13:16:21 -0500

    seccion7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,27 +2,53 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
+import { resolve } from 'path';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
   constructor(private http: HttpClient) {
      this.cargarProductos();
    }
 
    private cargarProductos() {
-     this.http.get('https://angularhtmldb.firebaseio.com/productos_idx.json').subscribe((res: Producto[]) => {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angularhtmldb.firebaseio.com/productos_idx.json').subscribe((res: Producto[]) => {
         this.productos = res;
         setTimeout(() => {
           this.cargando = false;
         }, 2000);
+        resolve();
         console.log(res);
      });
+       });
    }
    getPruducto(id: string) {
    return this.http.get(`https://angularhtmldb.firebaseio.com/productos/${id}.json`);
+   }
+
+   buscarProducto(termino: string) {
+     if (this.productos.length === 0) {
+        this.cargarProductos().then(() => {
+    this.filtrarProductos(termino);
+        });
+     } else {
+      this.filtrarProductos(termino);
+     }
+
+   }
+   private filtrarProductos(termino: string) {
+        console.log(this.productos);
+        this.productosFiltrado = [];
+        this.productos.forEach(prod => {
+           if (prod.categoria.indexOf(termino) >= 0) {
+               this.productosFiltrado.push(prod);
+           }
+        });
    }
 }
 
